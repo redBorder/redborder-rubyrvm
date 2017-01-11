90a6e05d05f6df4f7b2a1e49739c8593f2f12f3d
cjmateos <mateos83@gmail.com>
2017-01-11 11:27:06 +0100

    Add bundle gem

--- packaging/rpm/download_gems.sh
+++ packaging/rpm/download_gems.sh
@@ -2,6 +2,7 @@
 
 RVMVERSION=${RVMVERSION:="1.27.0"}
 BUNDLERVERSION=${BUNDLERVERSION:="1.12.5"}
+BUNDLEVERSION=${BUNDLEVERSION:="0.0.1"}
 RUBYGEMSVERSION=${RUBYGEMSVERSION:="2.4.8"}
 RUBYVERSION=${RUBYVERSION:="2.2.4"}
 
@@ -54,6 +55,7 @@ fi
 [ ! -f SOURCES/rvm-${RVMVERSION}.tar.gz ] && wget --no-check-certificate https://github.com/rvm/rvm/archive/${RVMVERSION}.tar.gz -O SOURCES/rvm-${RVMVERSION}.tar.gz
 [ ! -f SOURCES/ruby-${RUBYVERSION}.tar.bz2 ] && wget --no-check-certificate https://ftp.ruby-lang.org/pub/ruby/2.2/ruby-${RUBYVERSION}.tar.bz2 -O SOURCES/ruby-${RUBYVERSION}.tar.bz2
 [ ! -f SOURCES/rubygems-${RUBYGEMSVERSION}.tar.gz ] && wget --no-check-certificate https://rubygems.org/rubygems/rubygems-${RUBYGEMSVERSION}.tgz -O SOURCES/rubygems-${RUBYGEMSVERSION}.tar.gz
+[ ! -f SOURCES/bundle-${BUNDLEVERSION}.gem ] && wget --no-check-certificate https://rubygems.org/downloads/bundle-${BUNDLEVERSION}.gem -O SOURCES/bundle-${BUNDLEVERSION}.gem
 [ ! -f SOURCES/bundler-${BUNDLERVERSION}.gem ] && wget --no-check-certificate https://rubygems.org/downloads/bundler-${BUNDLERVERSION}.gem -O SOURCES/bundler-${BUNDLERVERSION}.gem
 
 exit 0
--- packaging/rpm/redborder-rubyrvm.spec
+++ packaging/rpm/redborder-rubyrvm.spec
@@ -13,7 +13,7 @@ License: ASL 2.0
 BuildArch: x86_64
 Source0: rvm-%{rvm_version}.tar.gz
 Source1: bundler-1.12.5.gem
-#Source2: bundle-0.0.1.gem
+Source2: bundle-0.0.1.gem
 Source3: chef-12.0.3.gem
 Source4: Gemfile_global
 Source5: Gemfile_web
@@ -68,7 +68,7 @@ default=ruby-%{ruby_version}
 
 # install bundle gem
 %{rvm_dir}/bin/rvm %{ruby_version}@global do gem install %{rvm_dir}/archives/bundler-*.gem
-#%{rvm_dir}/bin/rvm %{ruby_version}@global do gem install %{rvm_dir}/archives/bundle-*.gem
+%{rvm_dir}/bin/rvm %{ruby_version}@global do gem install %{rvm_dir}/archives/bundle-*.gem
 %{rvm_dir}/bin/rvm %{ruby_version}@global do gem install %{rvm_dir}/archives/chef-zero*.gem --no-ri
 %{rvm_dir}/bin/rvm %{ruby_version}@global do gem install %{rvm_dir}/archives/chef-*.gem --no-ri
 
